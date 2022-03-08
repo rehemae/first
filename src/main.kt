7d1fc69166f546688da7ca2a64bc8e41f73be9de
rehemae <rehemaephraimnanda@gmail.com>
2022-03-08 08:44:00 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name="Anna john"
+    println(name)
+
+    var age=20
+    println(age)
+
+    var phoneNumber:  String= "33289199"
+    println(phoneNumber)
+
+    var weight: Float=50f
+    println(weight)
+
+
+}
